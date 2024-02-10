e0dd79d0bd89fd123bcfc053fd6950b04e8b5990
kunndnn <44980639+kunndnn@users.noreply.github.com>
2024-02-10 18:37:38 +0530

    cards-components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import Food from "./Food";
+import Card from "./Card";
+function App() {
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      {/* <Header />
+      <Food />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default App;
