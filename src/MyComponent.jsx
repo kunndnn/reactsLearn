ab63ae5f797f5636a24361c286d55bae1cf843e3
kunndnn <44980639+kunndnn@users.noreply.github.com>
2024-02-25 16:37:50 +0530

    use effects with cleaner

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,31 +1,50 @@
+// useEffect()= react hook that tells react to do thiscode when(pick one):
+// this component re-renders
+// this component mounts
+// the state of a value
+
+// useEffect(function,[dependencies])
+
 // 1. useEffect(()=>{}) //runs after every re-render
 // 2. useEffect(()=>,[]) runs only on mount
 // 3. useEffect(()=>,[value]) //runs on mount + when value changes
-
+// uses:
+// #1 event listeners
+// #2 DOM manipulation
+// #3 subscriptions (real-time updates)
+// #4 fetching data from an api
+// #5 clean up when a component unmounts
 import { useState, useEffect } from "react";
 
 function MyComponents() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState("green");
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
   useEffect(() => {
-    document.title = `count: ${count} ${color}`;
-  }, [count, color]);
-  function addCount() {
-    setCount((c) => c + 1);
+    window.addEventListener("resize", handleResize);
+    console.log("event listener added");
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      console.log("event listener removed");
+    };
+  }, []);
+
+  useEffect(() => {
+    document.title = `size: ${width} x ${height}`;
+  }, [width, height]);
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
   }
-  function subtractCount() {
-    setCount((c) => c - 1);
-  }
-  function changeColor() {
-    setColor(color === "red" ? "green" : "red");
-  }
+
+  useEffect(() => {}, []);
+
   return (
     <>
-      <p style={{ color }}>Count :</p>
-      {count}
-      <button onClick={addCount}>Add</button>
-      <button onClick={subtractCount}>subtract</button>
-      <button onClick={changeColor}>Change Color</button>
+      <p>wiindow width :{width}</p>
+      <p>wiindow height :{height}</p>
     </>
   );
 }
